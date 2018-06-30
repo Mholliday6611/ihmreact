2716da3d12fb1c6f41922b81905b1def9d0463ca
mholliday6611 <mholliday6611@gmail.com>
2018-06-30 12:51:19 -0400

    remove node modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
+
+import Home from "./components/Home"
 
 const App = () => {
   return (
     <div>
-      <p>React here!</p>
+    	<Router>
+    		<Route exact path="/" component={Home} />
+    		<Route exact path="/login" component={login} />
+    	</Router>
     </div>
   );
 };
